6fc4e5b731c1e874d59f371ad44eaaff6d3f7329
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2023-05-20 16:54:26 -0300

    api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
 import { MoviesList } from "./components/MoviesList";
 import { MoviesContainer } from "./styles/MoviesContainer";
+import { MoviesService } from "./api/MoviesService";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const fetchMovies = async () => {
+    const data = await MoviesService.getMovies();
+    console.log(data)
+  }
+
+  useEffect(() => {
+    fetchMovies();
+
+  }, [])
+
   return (
     <div className="App">
       <Header />
